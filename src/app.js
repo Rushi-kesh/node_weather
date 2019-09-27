ad7dfd4cbeb5e728b00064a2aa9e95006cd95dfc
Rushi-kesh <gaikwadrushikesh88888@gmail.com>
2019-09-27 15:26:40 +0530

    second

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const path=require('path')
 const express= require('express');
 const app=express()
-
+const port=process.env.PORT||3000
 const dir=(path.join(__dirname,'../public'))
 
 app.use(express.static(dir))
@@ -15,6 +15,6 @@ app.get('/about',(req,res)=>{
 app.get('/weather',(req,res)=>{
     res.send("weather page");
 })
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("server is up");
 });
